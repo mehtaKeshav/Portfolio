51f898db910d4363a78559afc77ee3dd184607e9
Keshav Mehta <Keshav.Mehta@student.csulb.edu>
2023-08-23 19:02:21 -0700

    update

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -19,7 +19,10 @@ function Portfolio() {
             src: beachEvents1, 
             description: "Created a native web-based app to enrich students' campus socialization. It enables easy navigation to favorite events and keeps track of subscribed organizations' activities.",
             heading: "Beach Events",
-            style: "shadow-[#8f15b1]"
+            code: 'https://github.com/orgs/BeachEvents/teams/dev/repositories',
+            demo: '',
+            style: "shadow-[#8f15b1]",
+            textStyle: "hover:text-[#8f15b1]"
 
         },
         {
@@ -27,21 +30,30 @@ function Portfolio() {
             src:  FridgeManagerApp,
             description: "Revolutionized household food management with a Django-powered web app. Users can efficiently utilize their fridges, track expiry dates, and share and manage items seamlessly.",
             heading: "Fridge Manager",
-            style: "shadow-[#4d43c7]" 
+            code: 'https://github.com/mehtaKeshav/fridgeHelper',
+            demo: '',
+            style: "shadow-[#4d43c7]", 
+            textStyle: "hover:text-[#4d43c7]"
         },
         {
             id: 3, 
             src: EvilTwinAttack,
             description: "Created an automated Evil Twin attack software as a class project. It steals login information by setting up a replica Wi-Fi network and hosting a deceptive login page on an Apache server.",
             heading: "Evil Twin Attack Script",
-            style: "shadow-[#00c8ff]"
+            code: 'https://github.com/mehtaKeshav/Evil-Twin-Script',
+            demo: '',
+            style: "shadow-[#00c8ff]",
+            textStyle: "hover:text-[#00c8ff]"
         },
         {
             id: 4, 
             src: PokemonMain,
             description: "Crafted a backend program for a distinct Pokémon game app with a unique experience of hunting Pokémons and engaging in thrilling player battles.",
             heading: " Pokemon CLI Game",
-            style: "shadow-[#fdf5a2]"
+            code: 'https://github.com/mehtaKeshav/Pokemon_Game',
+            demo: '',
+            style: "shadow-[#fdf5a2]",
+            textStyle: "hover:text-[#fdf5a2]"
 
         },
         {
@@ -49,7 +61,10 @@ function Portfolio() {
             src: AutoPriceMLChart,
             description: "Created an ML model to predict the Prices of automobiles with an accuracy of 87% by applying processing data techniques on data fetched from UCI's Machine Learning Repository ",
             heading: " Auto price Predictor",
-            style: "shadow-[#fe8088]"
+            code: 'https://github.com/MayankTamakuwala/MachineLearning-Group-4',
+            demo: '',
+            style: "shadow-[#fe8088]",
+            textStyle: "hover:text-[#fe8088]"
         }
 
     ]
@@ -59,15 +74,15 @@ function Portfolio() {
         <p className='sm:mx-32 md:text-3xl text-xl font-semibold border-b-2 p-2 mx-2 border-gray-500'>Projects</p>
             <div className='grid sm:grid-cols-2 2xl:grid-cols-3 justify-center items-center pt-5 sm:pt-10 md:mx-40 mx-10 h-fit'>
                 {   
-                    portfolios.map(({id, src, description, heading, style}) => (
+                    portfolios.map(({id, src, description, heading, style, code, demo, textStyle}) => (
                         <div className={'shadow-lg hover:scale-105 rounded-xl mb-10  d:mb-10 sm:mx-3 p-2 '+ style}>
                             <p className=' font-semibold text-center'><h1>{heading}</h1></p>
                             <div key={id} className='sm:flex sm:flex-row sm:gap-4 md:h-[220px] py-2'>
                                 <div className=' sm:ml-2 grid grid-cols-1 sm:w-6/12'>
                                     <img src={src} alt="" className=' duration-200 h-[200px] sm:h-[140px] hover:scale-105 w-full rounded-xl'/>
-                                    <div className=' flex flex-row  gap-2'>
-                                            <button  className=' w-1/2  py-2  duration-200 hover:scale-110' >Demo</button>
-                                            <button className=' w-1/2   py-2  duration-200 hover:scale-110' >Code</button>
+                                    <div className=' flex flex-row gap-2'>
+                                            <a href={demo} className={' w-1/2  py-2 text-center duration-200 hover:scale-110 '+ textStyle} >Demo</a>
+                                            <a href={code} className={' w-1/2   py-2  text-center duration-200 hover:scale-110 '+ textStyle }>Code</a>
                                     </div>
                                 </div>
                                 <div className='sm:w-7/12 sm:px-2'>
